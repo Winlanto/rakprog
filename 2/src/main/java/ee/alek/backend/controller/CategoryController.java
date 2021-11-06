8cf54bc0c33feffbff7ecade7c3c5e2a1bc7c27c
Alek-Jaan Tsern <winlanto@tlu.ee>
2021-11-06 19:22:27 +0200

    java categories + items

---
+++ 2/src/main/java/ee/alek/backend/controller/CategoryController.java
@@ -0,0 +1,41 @@
+package ee.alek.backend.controller;
+
+import ee.alek.backend.model.Category;
+import ee.alek.backend.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+
+public class CategoryController {
+
+    @Autowired
+    CategoryService categoryService;
+
+    @GetMapping("categories")
+    public List<Category> getCategories() {
+        return categoryService.getCategories();
+    }
+
+    @PostMapping("categories")
+    public String postCategories(@RequestBody Category category) {
+        categoryService.saveCategory(category);
+        return "kategooria edukalt esitatud - " + category.getName()+", tüüp - "+category.getCategoryType();
+    }
+
+    @GetMapping("categories/{id}")
+    public String getCategory(@PathVariable Long id) {
+        return id+". "+categoryService.getCategory(id).getName()+", tüüp - "+categoryService.getCategory(id).getCategoryType();
+    }
+
+    @GetMapping("categories/delete/{id}")
+    public String deleteCategory(@PathVariable Long id){
+        String name = categoryService.getCategory(id).getName();
+        categoryService.deleteCategory(categoryService.getCategory(id));
+        return "category "+ id +" deleted - "+ name;
+    }
+
+}
+
---
+++ 2/src/main/java/ee/alek/backend/controller/ItemController.java
@@ -0,0 +1,41 @@
+package ee.alek.backend.controller;
+
+import ee.alek.backend.model.Item;
+import ee.alek.backend.service.ItemService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+
+public class ItemController {
+
+    @Autowired
+    ItemService itemService;
+
+    @GetMapping("items")
+    public List<Item> getItems() {
+        return itemService.getItems();
+    }
+
+    @PostMapping("items")
+    public String postItems(@RequestBody Item item) {
+        itemService.saveItem(item);
+        return "ese edukalt esitatud - " + item.getName();
+    }
+
+    @GetMapping("items/{id}")
+    public String getItem(@PathVariable Long id) {
+        return id+". "+itemService.getItem(id).getName();
+    }
+
+    @GetMapping("items/delete/{id}")
+    public String deleteItem(@PathVariable Long id){
+        String name = itemService.getItem(id).getName();
+        itemService.deleteItem(itemService.getItem(id));
+        return "item "+ id +" deleted - "+ name;
+    }
+
+}
+
